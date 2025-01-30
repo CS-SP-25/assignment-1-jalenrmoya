64f7ba423951890f0047a2e331d0bac955f99952
Jalen <jamoya@iu.edu>
2025-01-29 20:00:01 -0500

    Dynamic behavior added

--- SalesTaxCalculator.java
+++ SalesTaxCalculator.java
@@ -5,20 +5,19 @@ public class SalesTaxCalculator {
         State state; //variable to store the state object
 
         if (stateName.equalsIgnoreCase("Alaska")) { //if the state is Alaska
-            state = new Alaska(); //create a new Alaska object
+            state = new State(stateName, new NoTax()); //create a new state object with the name and the appropriate behavior
         }
         else if (stateName.equalsIgnoreCase("Indiana")) { //if the state is Indiana
-            state = new Indiana(); //create a new Indiana object
+            state = new State(stateName, new SevenPercent()); //create a new state object with the name and the appropriate behavior
         }
         else if (stateName.equalsIgnoreCase("Hawaii")) { //if the state is Hawaii
-            state = new Hawaii(); //create a new Hawaii object
+            state = new State(stateName, new FourPointFivePercent()); //create a new state object with the name and the appropriate behavior
         }
         else { //if the state is not Alaska or Indiana then print an error message
             System.out.println(stateName);
             System.out.println("Invalid state name " + stateName);
             return;
         }
-
         state.setName(stateName); //set the name of the state
         state.showTax(saleAmount); //show the sales tax for the state
     }
@@ -32,47 +31,16 @@ class State { //class to set up the state object
     public void setName(String name){ //method to set the name of the state
         this.name = name;
     }
+    public SalesTaxBehavior salesTaxBehavior;
 
     public void showTax(Double value) { //method to show the sales tax for the state
-        SalesTaxBehavior salesTaxBehavior; //variable to store the behavior for calculating sales tax
-
-        if (getName().equalsIgnoreCase("Alaska")) { //if the state is Alaska
-            salesTaxBehavior = new NoTax(); //set the behavior to NoTax
-        }
-        else if (getName().equalsIgnoreCase("Indiana")) { //if the state is Indiana
-            salesTaxBehavior = new SevenPercent(); //set the behavior to SevenPercent
-        }
-        else if (getName().equalsIgnoreCase("Hawaii")) { //if the state is Hawaii
-            salesTaxBehavior = new FourPercent(); //set the behavior to FourPercent
-        }
-        else { //if the state is not Alaska or Indiana
-            System.out.println("Invalid state name "); //print an error message
-            return; //exit the method
-        }
-
         double tax = salesTaxBehavior.compute(value); //calculate the sales tax using the appropriate behavior
         System.out.printf("The sales tax on $%.2f in %s is $%.2f.%n", value, getName(), tax); //print the sales tax on a value for the specific state
     }
-}
 
-class Alaska extends State { //class to set up the Alaska object
-    @Override //override the method in State
-    public void showTax(Double value) { //method to show the sales tax for Alaska
-        super.showTax(value);
-    }
-}
-
-class Indiana extends State { //class to set up the Indiana object
-    @Override //override the method in State
-    public void showTax(Double value) { //method to show the sales tax for Indiana
-        super.showTax(value);
-    }
-}
-
-class Hawaii extends State { //class to set up the Hawaii object
-    @Override //override the method in State
-    public void showTax(Double value) { //method to show the sales tax for Hawaii
-        super.showTax(value);
+    public State(String name, SalesTaxBehavior salesTaxBehavior) { //constructor for the State object
+        this.name = name; //set the name of the state
+        this.salesTaxBehavior = salesTaxBehavior; //set the behavior for calculating sales tax
     }
 }
 
@@ -94,9 +62,9 @@ class SevenPercent implements SalesTaxBehavior { //this method is used to calcul
     }
 }
 
-class FourPercent implements SalesTaxBehavior { //this method is used to calculate the amount of sales tax for a state that has a 4% sales tax
+class FourPointFivePercent implements SalesTaxBehavior { //this method is used to calculate the amount of sales tax for a state that has a 4% sales tax
     @Override //override the method in SalesTaxBehavior
     public Double compute(Double value) { //the method from SalesTaxBehavior but with the appropriate calculation for the state
-        return value * 0.04; //return the value multiplied by 0.04, as the sales tax is 4%
+        return value * 0.045; //return the value multiplied by 0.04, as the sales tax is 4%
     }
 }
